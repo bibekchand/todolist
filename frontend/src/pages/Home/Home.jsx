024f2f54f8a22dce09f0e4ff154fe70db2d4777c
Bibek Chand <bibekchand843@gmail.com>
2026-03-08 22:27:21 +0545

    Add login page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,124 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router"
+import ViewTask from "../../components/ViewTask/ViewTask.jsx";
+export default function App() {
+    const navigate = useNavigate()
+	const [searchedTaskList, setSearchedTaskList] = useState([]);
+	const [taskList, setTaskList] = useState([]);
+	function searchTasks(formData) {
+		const searchText = formData.get("searchText");
+		console.log(searchText);
+		axios
+			.get(`http://localhost:8000/searchTasks?searchText=${searchText}`)
+			.then((response) => {
+				console.log(response);
+				setSearchedTaskList(response.data);
+			})
+			.catch((error) => {
+				console.log("Error");
+			});
+	}
+	function fetchDataFromServer() {
+		console.log("Fetched data from server");
+		axios.get("http://localhost:8000/get_list/").then((response) => {
+			console.log(response);
+			setTaskList(response.data);
+		});
+	}
+	function postDataToServer(formData) {
+		axios
+			.post("http://localhost:8000/addTask", {
+				title: formData.get("title"),
+				description: formData.get("description"),
+				time: formData.get("time"),
+				status: formData.get("status") === "on" ? true : false,
+			})
+			.then(() => {
+				console.log("Sent data to server");
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		fetchDataFromServer();
+	}
+	useEffect(() => {
+		fetchDataFromServer();
+	}, []);
+	return (
+		<>
+			<form action={searchTasks}>
+				<div className="p-2 flex justify-center">
+					<input type="text" name="searchText" className="border-2" />
+					<button type="submit" className="border-2">
+						Search
+					</button>
+				</div>
+				{searchedTaskList.length !== 0 && (
+					<div className=" pl-2.5 text-2xl">Searched Tasks</div>
+				)}
+				{searchedTaskList?.map((item) => (
+					<ViewTask
+						key={item.id}
+						title={item.title}
+						description={item.description}
+						time={item.time}
+						status={item.status ? "Completed" : "Pending"}
+						showDeleteButton={false}
+					/>
+				))}
+			</form>
+			<div className="flex justify-center">
+				<form action={postDataToServer}>
+					<fieldset className="border-2 p-5 w-2.5 rounded-2xl flex flex-col gap-5 items-center">
+						<legend>Add Task</legend>
+						<label htmlFor="title">
+							Title:
+							<input name="title" id="title" className="border-2" />
+						</label>
+						<label htmlFor="description">
+							Description:
+							<textarea
+								name="description"
+								id="description"
+								className="border-2"
+								maxLength="200"
+							/>
+						</label>
+						<label htmlFor="time">
+							Time:
+							<input name="time" id="time" className="border-2" />
+						</label>
+						<label className="flex gap-2 justify-center item-center">
+							Status:
+							<input
+								type="checkbox"
+								name="status"
+								defaultChecked={true}
+								className="h-5 w-5"
+							/>
+						</label>
+						<button type="submit" className="cursor-pointer border-2 w-fit p-1">
+							Submit
+						</button>
+					</fieldset>
+				</form>
+			</div>
+
+			{taskList.length !== 0 && (
+				<div className=" pl-2.5 text-2xl">All Tasks</div>
+			)}
+			{taskList?.map((item) => (
+				<ViewTask
+					key={item.id}
+					id={item.id}
+					title={item.title}
+					description={item.description}
+					time={item.time}
+					status={item.status ? "Completed" : "Pending"}
+					fetchTaskList={fetchDataFromServer}
+				/>
+			))}
+		</>
+	);
+}
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function Login() {
+	const [loginFailed, setLoginFailed] = useState(false);
+	useEffect(() => {
+		if (loginFailed) {
+			setTimeout(() => {
+				setLoginFailed(false);
+			}, 2000);
+		}
+	}, [loginFailed]);
+	function login(formData) {
+		console.log(formData.get("username"));
+		axios
+			.post(
+				"http://localhost:8000/login",
+				{
+					username: formData.get("username"),
+					password: formData.get("password"),
+				},
+				{
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+				},
+			)
+			.then((response) => {
+				console.log(response.data);
+                token = response.data.access_token
+                localStorage.setItem("token", token)
+                setLoginFailed(false)
+				console.log("Logged in successfully");
+			})
+			.catch((error) => {
+				console.log(error);
+                setLoginFailed(true)
+				console.log("Login failed");
+			});
+	}
+	return (
+		<div className="flex justify-center p-2">
+			<form action={login} className="flex flex-col gap-2 justify-center">
+				<input
+					type="text"
+					placeholder="Username"
+					name="username"
+					className="border-2 p-0.5"
+				/>
+				<input
+					type="text"
+					placeholder="Password"
+					name="password"
+					className="border-2 p-0.5"
+				/>
+				<button
+					type="submit"
+					className="border-2 shadow shadow-amber-800 active:shadow-amber-50"
+				>
+					Login
+				</button>
+				{loginFailed && <div className="text-red-500">Login Failed</div>}
+			</form>
+		</div>
+	);
+}
