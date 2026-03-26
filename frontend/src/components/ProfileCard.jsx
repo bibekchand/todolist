f465dd59df38c48e2cf42f3cba5dc4d6d9a4ba3b
Bibek Chand <bibekchand843@gmail.com>
2026-03-26 17:36:11 +0545

    WIP: profile card

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,8 +1,9 @@
+import swiftLogo from "../assests/swift_logo.png";
 export default function ProfileCard() {
 	return (
 		<div className="h-fit w-fit border-2 border-black m-auto relative rounded overflow-hidden">
 			<div className="h-20 w-20  border-black bg-red-500 -mb-10"></div>
-			<div className="h-10 w-10  top-5 left-5 border-2 rounded-full border-blue-500 z-50 absolute -mb-10 bg-red-500"></div>
+			<img src={swiftLogo} alt="Profile" className="h-10 w-10 border-2 border-black rounded-full overflow-hidden absolute left-5 top-5"/>
 			<div className="h-20 w-20  border-t-2 bg-black"></div>
 		</div>
 	);
