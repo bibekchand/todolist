36903feb6dc6ba37fbaec4a3997b6f9dab20555c
Bibek Chand <bibekchand843@gmail.com>
2026-04-22 18:56:27 +0545

    WIP: Sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,44 +1,26 @@
-import { useState, useEffect } from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-export default function App() {
-    const [collapsed, setCollapsed] = useState(false);
-    const [toggled, setToggled] = useState(true);
+import Sidebar from "../assests/sidebar.svg?react";
+import NotificationIcon from "../assests/notification.svg?react";
+import catImage from "../assests/cato.avif";
+import { useState } from "react";
+export default function Home() {
+    const [username, setUsername] = useState("Ram Shah");
     return (
-        <>
-            <div className="flex">
-                <Sidebar
-                    className="h-full"
-                    toggled={toggled}
-                    collapsed={collapsed}
-                    collapsedWidth="0"
-                    breakPoint="md"
-                    onBackdropClick={() => {
-                        setToggled(false);
-                    }}
-                >
-                    <div className="text-4xl">
-                        Something
-                        <div>
-                            {
-                                <button type="button" onClick={() => setCollapsed(!collapsed)}>
-                                    Toggle
-                                </button>
-                            }
+        <div className="grid grid-cols-[auto_1fr]">
+            <div className="flex border-2 p-2 flex-col gap-5">
+                <div className="flex items-center gap-5">
+                    <div className="flex rounded-2xl items-center gap-2 p-2 hover:bg-gray-500 duration-200 ease-in-out cursor-pointer">
+                        <div className="h-10 w-10 rounded-full overflow-clip">
+                            <img src={catImage} alt="" />
                         </div>
+                        <span>{username}</span>
+                        <span className="rotate-90"> &gt;</span>
                     </div>
-
-                    <Menu>
-                        <SubMenu label="Charts" icon={<div>😊</div>}>
-                            <MenuItem className=""> Pie charts </MenuItem>
-                            <MenuItem className=""> Something </MenuItem>
-                        </SubMenu>
-                        <MenuItem> Documentation </MenuItem>
-                        <MenuItem> Calendar </MenuItem>
-                    </Menu>
-                </Sidebar>
+                    <NotificationIcon className="fill-black  hover:fill-gray-300 duration-100 ease-in-out cursor-pointer" />
+                    <Sidebar className="fill-black hover:fill-gray-300  duration-100 ease-in-out cursor-pointer" />
+                </div>
                 <div>Hello world</div>
             </div>
-            <div>Hello world</div>
-        </>
+            <div className="border-2">Hello</div>
+        </div>
     );
 }
